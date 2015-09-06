04b51bee76d246555f4d86572d581aebbe70873f
Frédéric Tuong <frederic.tuong@lri.fr>
2015-09-06 14:43:18 +0200

    update author's information from the papers or easychair

--- core/html/body/program.ml
+++ core/html/body/program.ml
@@ -42,18 +42,37 @@ module Program : PAGE = struct
   let file = "program.html"
   let title = "Program"
 
-  let l = BatList.map (function time, l -> 
+  module L = BatList
+
+  let l = L.map (function time, l -> 
             let l = 
-              BatList.map (function time, l_author, title, l ->
-                             let l_author = BatList.map (function motif, (author, None) -> <:html< <span>$str: motif $</span><span style="color:#337AB7">$str: author $</span> >>
-                                                                | motif, (author, Some ln) -> <:html< <span>$str: motif $</span><a href=$str: ln $>$str: author$</a> >>) l_author in
-                             let l = BatList.map (function name, ln -> <:html< <a href=$str: ln $>$str: name$</a> >>) l in
+              L.map (function time, l_author, title, l ->
+                             let l_author_info, l = L.partition (function "author_info", _ -> true | _ -> false) l in
+                             let l_author, (l_author_info1, l_author_info2) = 
+                               let l1, l2 =
+                                 let pat = ", " in
+                                 L.split (L.map2 (fun (_, info) author ->
+                                                    let motif, author = 
+                                                      match author with motif, (author, None) ->
+                                                                          motif, <:html< <span style="color:#337AB7">$str: author $</span> >>
+                                                                      | motif, (author, Some ln) ->
+                                                                          motif, <:html< <span><a href=$str: ln $>$str: author$</a></span> >> in
+                                                    let info1, info2 = BatString.rsplit info ~by:pat in
+                                                    <:html< <td style="width:3em;background-color:#F8F8F8;color:#727272">$str: motif $</td><td style="font-size:120%;padding-left:0.5em;padding-right:0.5em">$list: [author] $</td> >>,
+                                                    (<:html< <td style="background-color:#F8F8F8"></td><td style="padding-left:0.5em;padding-right:0.5em">$str: info1 $</td> >>,
+                                                    <:html< <td style="background-color:#F8F8F8"></td><td style="color:#727272;padding-left:0.5em;padding-right:0.5em">$str: info2 $</td> >>))
+                                                 (match l_author_info, l_author with [], [_] -> ["", pat]
+                                                                                   | _ when L.length l_author_info = L.length l_author -> l_author_info)
+                                                 l_author) in
+                               l1, L.split l2 in
+                             let l = L.map (function name, ln -> <:html< <a href=$str: ln $><b>$str: name$</b></a> >>) l in
                              <:html< <li><div style="color:#727272">$str: time $</div>
-                                         $list: l_author $
-                                         <div><b>$str: title $</b></div>
-                                         $list: l$</li> >>) l in
-            <:html< <h4>$str: time $</h4><div><ul>$list: l $</ul></div> >>) CFP.program
+                                         <div style="font-size:120%"><b>$str: title $</b></div>
+                                         $list: l$
+                                         <table style="text-align:center"><tbody><tr>$list: l_author $</tr><tr>$list: l_author_info1 $</tr><tr>$list: l_author_info2 $</tr></tbody></table></li> >>) l in
+            <:html< <hr></hr><h4>$str: time $</h4><div><ul style="padding-left:8em">$list: l $</ul></div> >>) CFP.program
   let body = body ("Program Overview", <:html<
+<a href="http://cruise.eecs.uottawa.ca/models2015/workshops.html"><b>Workshop Room: Panorama</b></a>
 $list: l $
 >>)
 end
